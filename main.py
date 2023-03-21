5ec091427b043763172facedc20634d9cc358e2f
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-03-21 05:16:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('$hello'):
-        await message.channel.send('Привет! Я бот!')
+        await message.channel.send('Hi! I am a bot!')
     elif message.content.startswith('$smile'):
         await message.channel.send(gen_emodji())
     elif message.content.startswith('$coin'):
